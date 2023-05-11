cf792e34ad1fd247b3380f4148bc0a443bece36e
Sanjeevkumar M <130639043+Sanjeevkumar2004@users.noreply.github.com>
2023-05-11 18:46:28 +0530

    Add files via upload

---
+++ basic program/Solve equation.py
@@ -0,0 +1,7 @@
+x=int(input("enter a number"))
+y=int(input("enter a number"))
+if(x<y):
+   z=(y-x)*(x+y)
+else:
+   z=(x-y)*(x+y)
+   print(z)
---
+++ basic program/surface area & volume.py
@@ -0,0 +1,8 @@
+r=int(input("enter a radius of cylinder"))
+h=int(input("enter a height of cylinder"))
+pi=3.14
+v=pi*((r*r)*h)
+s=((2*pi*r)*h)+((pi*r**2)*2)
+print("volume is",v)
+print("surface area of cylinder is",s)
+
